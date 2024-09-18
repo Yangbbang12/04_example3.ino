9d22ec6f752e04d172d4c9db1a003925925ad602
Yangbbang12 <135795237+Yangbbang12@users.noreply.github.com>
2024-09-18 22:21:21 +0900

    Create 04_example3.ino

---
+++ 04_example3.ino
@@ -0,0 +1,14 @@
+#define PIN_LED 13
+unsigned int count, toggle;
+
+void setup() {
+   pinMode(PIN_LED, OUTPUT);
+   Serial.begin(115200);
+   while (!Serial){
+    ;
+   }
+   Serial.println("Hello World!");
+   count = toggle = 0;
+   digitalWrite(PIN_LED, toggle);
+   
+}
